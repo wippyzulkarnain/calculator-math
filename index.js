7da129049896a61ec25c867ec7f2da2768bed962
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-22 18:54:23 +0700

    with dom

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
-var value1=parseInt(prompt("Enter value 1"))
-var value2=parseInt(prompt("Enter value 2"))
+const   btn=document.querySelectorAll(".btn")
+const display=document.getElementById("display")
+const btnc = document.getElementById("btnc")
+const btnequal = document.getElementById("btnequal")
+let a="";
+let b="";
+let op="";
+let result;
+btnc.addEventListener('click', cleardisplay)
+btnequal.addEventListener('click' , commence)
 
-function add(a,b){
-    return hasil=a+b
-}
-function multiply(a,b){
-    return hasil=a*b
-}
-function substract(a,b) {
-    return hasil=a-b
-}
-function divide(a,b) {
-    return hasil=a/b
-}
-function modulo(a,b) {
-    return hasil=a%b
+function cleardisplay(){
+    display.value =""
+    a="";
+    b="";
 }
 
-console.log(value1+" + "+value2+" = "+add(value1,value2))
-console.log(value1+" * "+value2+" = "+multiply(value1,value2))
-console.log(value1+" - "+value2+" = "+substract(value1,value2))
-console.log(value1+" / "+value2+" = "+divide(value1,value2))
-console.log(value1+" % "+value2+" = "+modulo(value1,value2))
+btn.forEach(function (h) {
+    h.addEventListener('click', add)
+})
+
+
+function add(e){
+display.value += e.target.innerHTML
+}
+
+const   btnop=document.querySelectorAll(".btnop")
+btnop.forEach(function (j) {
+    j.addEventListener('click', operator)
+})
+
+function operator(k){
+if (a===""){
+        a = display.value
+        op = k.target.innerHTML
+        display.value=""
+}
+        else{
+            b= display.value
+            op = k.target.innerHTML
+            display.value=""
+        }
+}
+
+function commence(){
+    b = display.value
+    a = parseFloat(a)
+    b = parseFloat(b)
+    if (op == "+"){
+        display.value = a+b
+        a= display.value
+    }
+    else if (op == "/"){
+        display.value = a/b
+        a= display.value
+    }
+    else if (op == "-"){
+        display.value = a-b
+        a= display.value
+    }
+    else if (op == "*"){
+        display.value = a*b
+        a= display.value
+    }
+}
